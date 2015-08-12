753aefe945be33a8fbb72529f3d5c8a7b8ab2fa8
Jonas Bornold <jonas.bornold@gmail.com>
2015-08-12 16:38:35 +0200

    Character animation initial commit

---
+++ Character Animation/Skeleton Builder/Helpers/TreeNode.cs
@@ -0,0 +1,145 @@
+﻿/*
+Copyright https://code.google.com/p/yet-another-tree-structure/ <gt4dev@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/*
+ * NOTICE: File changed from orignial work
+ * 2015 Qualisys AB
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace QualisysRealTime.Unity.Skeleton
+{
+    public class TreeNode<T> : IEnumerable<TreeNode<T>>
+    {
+        /// <summary>
+        /// The data contained in the TreeNode
+        /// </summary>
+        public T Data { get; set; }
+        /// <summary>
+        /// A pointer to the parent Node
+        /// </summary>
+        public TreeNode<T> Parent { get; set; }
+        /// <summary>
+        /// A collection of children Node
+        /// </summary>
+        public TreeNode<T>[] Children { get; set; }
+        /// <summary>
+        /// Returns if Node has no Parent 
+        /// </summary>
+        public Boolean IsRoot
+        {
+            get { return Parent == null; }
+        }
+        /// <summary>
+        /// Returns true if Node has no Children
+        /// </summary>
+        public Boolean IsLeaf
+        {
+            get { return Children.Length == 0; }
+        }
+
+        /// <summary>
+        /// Constructor for a new node
+        /// </summary>
+        /// <param name="data">The Data the node should contain</param>
+        public TreeNode(T data)
+        {
+            this.Data = data;
+            this.Children = new TreeNode<T>[0];
+        }
+
+        /// <summary>
+        /// Adding a new node as child to this node
+        /// </summary>
+        /// <param name="child">The Data of the child node</param>
+        /// <returns>A reference to the child node</returns>
+        public TreeNode<T> AddChild(T child)
+        {
+            TreeNode<T> childNode = new TreeNode<T>(child) { Parent = this };
+            var temp = new TreeNode<T>[Children.Length + 1];
+            Array.Copy(Children, temp, Children.Length);
+            temp[temp.Length-1] = childNode;
+            Children = temp;
+            return childNode;
+        }
+
+        /// <summary>
+        /// Apply an action to all the Data in the tree
+        /// </summary>
+        /// <param name="action">The action to be applied on the Data</param>
+        public void Traverse(Action<T> action)
+        {
+            action(Data);
+            foreach (var child in Children)
+                child.Traverse(action);
+        }
+        /// <summary>
+        /// Apply and action on all the Nodes in the tree
+        /// </summary>
+        /// <param name="action">The action on wich to be applied to the tree</param>
+        public void Traverse(Action<TreeNode<T>> action)
+        {
+            action(this);
+            foreach (var child in Children)
+                child.Traverse(action);
+        }
+
+        public override string ToString()
+        {
+            return Data != null ? Data.ToString() : "[data null]";
+        }
+
+
+        #region searching
+        
+
+        /// <summary>
+        /// Returns the first TreeNode of which the first predicate is true, otherwise default
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public TreeNode<T> FindTreeNode(Func<TreeNode<T>, bool> predicate)
+        {
+            if (predicate(this)) return this;
+            else for (int i = 0; i < Children.Length; i++)
+                {
+                    var res = Children[i].FindTreeNode(predicate);
+                    if (res != null) return res;
+                }
+            return null;
+        }
+        #endregion
+
+
+        #region iterating
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<TreeNode<T>> GetEnumerator()
+        {
+            yield return this;
+            for (int i = 0; i < this.Children.Length; i++)
+                foreach (var anyChild in this.Children[i])
+                    yield return anyChild;
+        }
+        #endregion
+    }
+}
---
+++ Streaming/RTCharacterStream.cs
@@ -0,0 +1,352 @@
+﻿#region --- LINCENSE ---
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2015 Qualisys AB
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#endregion
+
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable 0649
+namespace QualisysRealTime.Unity.Skeleton
+{
+    class RTCharacterStream : MonoBehaviour
+    {
+        public string MarkerPrefix = "";
+        public bool UseFingers = false;
+        private bool fingerUse;
+        public bool ScaleToModel = false;
+        public bool LockPosition = false;
+        public bool Reset = false;
+        public CharactersModel model = CharactersModel.Model1;
+        public BoneRotations boneRotatation = new Model1();
+        public Debugging debug;
+
+        private RTClient rtClient;
+        private Vector3 pos;
+        private bool streaming = false;
+        private List<LabeledMarker> markerData;
+        private SkeletonBuilder skeletonBuilder;
+        private BipedSkeleton skeleton;
+        private CharacterGameObjects charactersJoints = new CharacterGameObjects();
+        private CharactersModel cf = CharactersModel.Model1;
+        private float scale = 0;
+        private bool jointsFound = false;
+        /// <summary>
+        /// Locating the joints of the character and find the scale by which the position should be applied to
+        /// </summary>
+        public void Start()
+        {
+            rtClient = RTClient.GetInstance();
+            fingerUse = UseFingers;
+            charactersJoints.SetLimbs(this.transform, fingerUse);
+            if (!charactersJoints.IsAllJointsSet(fingerUse))
+            {
+                charactersJoints.PrintAll();
+                UnityEngine.Debug.LogError("Could not find all necessary joints");
+            }
+            else jointsFound = true;
+            
+            var animation = this.GetComponent<Animation>();
+            if (animation) animation.enabled = false;
+        }
+        /// <summary>
+        /// Updates the rotation and position of the Character
+        /// </summary>
+        public void Update()
+        {
+            if (!jointsFound) return;
+            if (rtClient == null) rtClient = RTClient.GetInstance();
+            streaming = rtClient.GetStreamingStatus();
+            if (!streaming && !LockPosition) return;
+            markerData = rtClient.Markers;
+            if ((markerData == null || markerData.Count == 0) && !LockPosition)
+            {
+                UnityEngine.Debug.LogError("The stream does not contain any markers");
+                return;
+            }
+            if (Reset || skeletonBuilder == null)
+            {
+                if (fingerUse != UseFingers)
+                {
+                    fingerUse = UseFingers;
+                    charactersJoints.SetLimbs(this.transform, fingerUse);
+                }
+                scale = 0;
+                skeletonBuilder = new SkeletonBuilder();
+                skeletonBuilder.MarkerPrefix = MarkerPrefix;
+                skeleton = new BipedSkeleton();
+                Reset = false;
+            }
+            if (!LockPosition)
+            {
+                if (debug!= null) skeletonBuilder.SolveWithIK = debug.UseIK;
+                skeleton = skeletonBuilder.SolveSkeleton(markerData);
+            }
+            if (scale == 0 && ScaleToModel) scale = FindScale(charactersJoints.pelvis);
+            SetModelRotation();
+            SetAll();
+        }
+         
+        /// <summary>
+        /// Maps the built skeleton joint rotation to the characeters joints
+        /// </summary>
+        private void SetAll()
+        {
+            foreach (var b in skeleton.Root)
+            {
+                switch (b.Data.Name)
+                {
+                    case Joint.PELVIS:
+                        SetJointRotation(charactersJoints.pelvis, b.Data, boneRotatation.hip);
+                        if (charactersJoints.pelvis && !b.Data.Pos.IsNaN())
+                        {
+                            Vector3 positionChange = ScaleToModel ?
+                                (b.Data.Pos * scale * transform.localScale.magnitude).Convert()
+                                : b.Data.Pos.Convert();
+                            charactersJoints.pelvis.position = transform.position + positionChange;
+                        }
+                        break;
+                    case Joint.SPINE0:
+                        if (charactersJoints.spine.Length > 0)
+                            SetJointRotation(charactersJoints.spine[0], b.Data, boneRotatation.spine);
+                        break;
+                    case Joint.SPINE1:
+                        if (charactersJoints.spine.Length > 1)
+                            SetJointRotation(charactersJoints.spine[1], b.Data, boneRotatation.spine);
+                        break;
+                    case Joint.SPINE3:
+                        if (charactersJoints.spine.Length > 2)
+                            SetJointRotation(charactersJoints.spine[2], b.Data, boneRotatation.spine);
+                        break;
+                    case Joint.NECK:
+                        SetJointRotation(charactersJoints.neck, b.Data, boneRotatation.neck);
+                        break;
+                    case Joint.HEAD:
+                        SetJointRotation(charactersJoints.head, b.Data, boneRotatation.head);
+                        break;
+                    case Joint.HIP_L:
+                        SetJointRotation(charactersJoints.leftThigh, b.Data, boneRotatation.legUpperLeft);
+                        break;
+                    case Joint.HIP_R:
+                        SetJointRotation(charactersJoints.rightThigh, b.Data, boneRotatation.legUpperRight);
+                        break;
+                    case Joint.KNEE_L:
+                        SetJointRotation(charactersJoints.leftCalf, b.Data, boneRotatation.legLowerLeft);
+                        break;
+                    case Joint.KNEE_R:
+                        SetJointRotation(charactersJoints.rightCalf, b.Data, boneRotatation.legLowerRight);
+                        break;
+                    case Joint.FOOTBASE_L:
+                        SetJointRotation(charactersJoints.leftFoot, b.Data, boneRotatation.footLeft);
+                        break;
+                    case Joint.FOOTBASE_R:
+                        SetJointRotation(charactersJoints.rightFoot, b.Data, boneRotatation.footRight);
+                        break;
+                    case Joint.CLAVICLE_L:
+                        SetJointRotation(charactersJoints.leftClavicle, b.Data, boneRotatation.clavicleLeft);
+                        break;
+                    case Joint.CLAVICLE_R:
+                        SetJointRotation(charactersJoints.rightClavicle, b.Data, boneRotatation.clavicleRight);
+                        break;
+                    case Joint.SHOULDER_L:
+                        SetJointRotation(charactersJoints.leftUpperArm, b.Data, boneRotatation.armUpperLeft);
+                        break;
+                    case Joint.SHOULDER_R:
+                        SetJointRotation(charactersJoints.rightUpperArm, b.Data, boneRotatation.armUpperRight);
+                        break;
+                    case Joint.ELBOW_L:
+                        SetJointRotation(charactersJoints.leftForearm, b.Data, boneRotatation.armLowerLeft);
+                        break;
+                    case Joint.ELBOW_R:
+                        SetJointRotation(charactersJoints.rightForearm, b.Data, boneRotatation.armLowerRight);
+                        break;
+                    case Joint.WRIST_L:
+                        SetJointRotation(charactersJoints.leftHand, b.Data, boneRotatation.handLeft);
+                        break;
+                    case Joint.WRIST_R:
+                        SetJointRotation(charactersJoints.rightHand, b.Data, boneRotatation.handRight);
+                        break;
+                    case Joint.HAND_L:
+                        if (fingerUse && charactersJoints.fingersLeft != null) 
+                            foreach (var fing in charactersJoints.fingersLeft) 
+                                SetJointRotation(fing, b.Data, boneRotatation.fingersLeft);
+                        break;
+                    case Joint.HAND_R:
+                        if (fingerUse && charactersJoints.fingersRight != null) 
+                            foreach (var fing in charactersJoints.fingersRight) 
+                                SetJointRotation(fing, b.Data, boneRotatation.fingersRight);
+                        break;
+                    case Joint.TRAP_L:
+                        if (fingerUse) 
+                            SetJointRotation(charactersJoints.thumbLeft, b.Data, boneRotatation.thumbLeft);
+                        break;
+                    case Joint.TRAP_R:
+                        if (fingerUse) 
+                            SetJointRotation(charactersJoints.thumbRight, b.Data, boneRotatation.thumbRight);
+                        break;
+                    case Joint.ANKLE_L:
+                    case Joint.ANKLE_R:
+                    default:
+                        break;
+                }
+            }
+        }
+        /// <summary>
+        /// Find the scale by which the characters positions should change
+        /// </summary>
+        /// <param name="pelvis">The pelvis where the root position change is applied</param>
+        /// <returns>A scaling factor to be applied to the poistional vector</returns>
+        private float FindScale(Transform pelvis)
+        {
+            float pelvisHeight = 0;
+            var trans = pelvis;
+            while (trans && trans != this.transform) {
+                pelvisHeight += trans.localPosition.y;
+                trans = trans.parent;
+            } 
+            float s = pelvisHeight / skeleton.Root.Data.Pos.Y;
+            s /= transform.localScale.magnitude;
+            return s;
+        }
+        /// <summary>
+        /// Sets the rotation of the Transform from Bone object to 
+        /// </summary>
+        /// <param name="go">Joint Transform</param>
+        /// <param name="b">Bone</param>
+        /// <param name="euler">Euler angels</param>
+        private void SetJointRotation(Transform go, Bone b, Vector3 euler)
+        {
+            if (b != null && !b.Orientation.IsNaN() && go)
+            {
+                go.rotation =
+                    transform.rotation
+                    * b.Orientation.Convert()
+                    * Quaternion.Euler(euler)
+                    * Quaternion.Euler(boneRotatation.root);
+            }
+        }
+        /// <summary>
+        /// Checks whether the model has been changed since last and change the model
+        /// </summary>
+        private void SetModelRotation()
+        {
+            if (cf != model)
+            {
+                switch (model)
+                {
+                    case CharactersModel.Model1:
+                        boneRotatation = new Model1();
+                        break;
+                    case CharactersModel.Model2:
+                        boneRotatation = new Model2();
+                        break;
+                    case CharactersModel.Model3:
+                        boneRotatation = new Model3();
+                        break;
+                    case CharactersModel.Model4:
+                        boneRotatation = new Model4();
+                        break;
+                    case CharactersModel.Model5:
+                        boneRotatation = new Model5();
+                        break;
+                    case CharactersModel.Model6:
+                        boneRotatation = new Model6();
+                        break;
+                    case CharactersModel.Model7:
+                        boneRotatation = new Model7();
+                        break;
+                    case CharactersModel.EmptyModel:
+                        boneRotatation = new Empty();
+                        break;
+                    default:
+                        break;
+                }
+                cf = model;
+            }
+        }
+        void OnDrawGizmos()
+        {
+            if ( Application.isPlaying && 
+                (streaming && debug != null && markerData != null) 
+                || LockPosition)
+            {
+                pos = this.transform.position + debug.Offset;
+                if (debug.markers.ShowMarkers)
+                {
+                    foreach (var lb in markerData)
+                    {
+                        Gizmos.color = new Color(lb.Color.r, lb.Color.g, lb.Color.b);
+                        Gizmos.DrawSphere(lb.Position + pos, debug.markers.MarkerScale);
+                    }
+                }
+
+                if (debug.markers.MarkerBones && rtClient.Bones != null)
+                {
+                    foreach (var lb in rtClient.Bones)
+                    {
+                        var from = markerData.Find(md => md.Label == lb.From).Position + pos;
+                        var to = markerData.Find(md => md.Label == lb.To).Position + pos;
+                        Debug.DrawLine(from, to, debug.markers.boneColor);
+                    }
+                }
+                
+                if (skeleton != null && 
+                    (debug.showSkeleton || debug.showRotationTrace || debug.showJoints || debug.showConstraints || debug.showTwistConstraints))
+                {
+                    Gizmos.color = debug.jointColor;
+                    foreach (TreeNode<Bone> b in skeleton.Root)
+                    {
+                        if (debug.showSkeleton)
+                        {
+                            foreach (TreeNode<Bone> child in b.Children)
+                            {
+                                UnityEngine.Debug.DrawLine(b.Data.Pos.Convert() + pos, child.Data.Pos.Convert() + pos, debug.skelettColor);
+                            }
+                        }
+                        if (debug.showRotationTrace && (!b.IsLeaf))
+                        {
+                            UnityDebug.DrawRays(b.Data.Orientation, b.Data.Pos.Convert() + pos, debug.traceLength);
+                        }
+                        if (debug.showJoints)
+                        {
+                            Gizmos.DrawSphere(b.Data.Pos.Convert() + pos, debug.jointSize);
+                        }
+                        if ((debug.showConstraints || debug.showTwistConstraints) && b.Data.HasConstraints)
+                        {
+                            OpenTK.Quaternion parentRotation =
+                                b.Parent.Data.Orientation * b.Data.ParentPointer;
+                            OpenTK.Vector3 poss = b.Data.Pos + pos.Convert();
+                            if (debug.showConstraints)
+                            {
+                                UnityDebug.CreateIrregularCone(
+                                    b.Data.Constraints, poss,
+                                    OpenTK.Vector3.NormalizeFast(
+                                        OpenTK.Vector3.Transform(OpenTK.Vector3.UnitY, parentRotation)),
+                                    parentRotation,
+                                    50,//debug.jointsConstrains.coneResolution,
+                                    debug.traceLength//debug.jointsConstrains.coneSize
+                                    );
+                            }
+                            if (debug.showTwistConstraints)
+                            {
+                                UnityDebug.DrawTwistConstraints(b.Data, b.Parent.Data, poss, debug.traceLength);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
