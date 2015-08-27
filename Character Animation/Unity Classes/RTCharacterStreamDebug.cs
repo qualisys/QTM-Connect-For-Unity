428a2c9903946f16748016aa7259019f75f48333
Jonas Bornold <jonas.bornold@gmail.com>
2015-08-27 10:37:35 +0200

    Adding CharacterStreamDebug Class

---
+++ Character Animation/Unity Classes/RTCharacterStreamDebug.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+namespace QualisysRealTime.Unity.Skeleton
+{
+    class RTCharacterStreamDebug : RTCharacterStream
+    {
+        public Skeleton skeleton;
+
+        void OnDrawGizmos()
+        {
+            if (Application.isPlaying 
+                && rtClient.GetStreamingStatus() 
+                && skeleton != null 
+                && markerData != null)
+            {
+                pos = this.transform.position + skeleton.Offset;
+                if (skeleton.markers.ShowMarkers)
+                {
+                    foreach (var lb in markerData)
+                    {
+                        Gizmos.color = new Color(lb.Color.r, lb.Color.g, lb.Color.b);
+                        Gizmos.DrawSphere(lb.Position + pos, skeleton.markers.MarkerScale);
+                    }
+                }
+
+                if (skeleton.markers.MarkerBones && rtClient.Bones != null)
+                {
+                    foreach (var lb in rtClient.Bones)
+                    {
+                        var from = markerData.Find(md => md.Label == lb.From).Position + pos;
+                        var to = markerData.Find(md => md.Label == lb.To).Position + pos;
+                        Debug.DrawLine(from, to, skeleton.markers.boneColor);
+                    }
+                }
+
+                if (base.skeleton != null &&
+                    (skeleton.showSkeleton || skeleton.showRotationTrace || skeleton.showJoints || skeleton.showConstraints || skeleton.showTwistConstraints))
+                {
+                    Gizmos.color = skeleton.jointColor;
+                    foreach (TreeNode<Bone> b in base.skeleton.Root)
+                    {
+                        if (skeleton.showSkeleton)
+                        {
+                            foreach (TreeNode<Bone> child in b.Children)
+                            {
+                                UnityEngine.Debug.DrawLine(b.Data.Pos.Convert() + pos, child.Data.Pos.Convert() + pos, skeleton.skelettColor);
+                            }
+                        }
+                        if (skeleton.showRotationTrace && (!b.IsLeaf))
+                        {
+                            UnityDebug.DrawRays(b.Data.Orientation, b.Data.Pos.Convert() + pos, skeleton.traceLength);
+                        }
+                        if (skeleton.showJoints)
+                        {
+                            Gizmos.DrawSphere(b.Data.Pos.Convert() + pos, skeleton.jointSize);
+                        }
+                        if ((skeleton.showConstraints || skeleton.showTwistConstraints) && b.Data.HasConstraints)
+                        {
+                            OpenTK.Quaternion parentRotation =
+                                b.Parent.Data.Orientation * b.Data.ParentPointer;
+                            OpenTK.Vector3 poss = b.Data.Pos + pos.Convert();
+                            if (skeleton.showConstraints)
+                            {
+                                UnityDebug.CreateIrregularCone(
+                                    b.Data.Constraints, poss,
+                                    OpenTK.Vector3.NormalizeFast(
+                                        OpenTK.Vector3.Transform(OpenTK.Vector3.UnitY, parentRotation)),
+                                    parentRotation,
+                                    skeleton.coneResolution,
+                                    skeleton.coneSize
+                                    );
+                            }
+                            if (skeleton.showTwistConstraints)
+                            {
+                                UnityDebug.DrawTwistConstraints(b.Data, b.Parent.Data, poss, skeleton.traceLength);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Character Animation/Unity Classes/RTCharacterStreamDebugEditor .cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace QualisysRealTime.Unity.Skeleton
+{ 
+
+    [CustomEditor(typeof(RTCharacterStreamDebug))]
+    public class RTCharacterStreamDebugEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            RTCharacterStreamDebug characterStream = (RTCharacterStreamDebug)target;
+            if (GUILayout.Button("Reset Skeleton"))
+            {
+                characterStream.ResetSkeleton();
+            }
+            if (GUILayout.Button("Set model Skeleton"))
+            {
+                characterStream.SetModelRotation();
+            }
+        }
+    }
+}
