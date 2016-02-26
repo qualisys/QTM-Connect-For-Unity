52b6c399053668647b86b886e5d6f3e7098e4dbb
Alec <frstrtr@gmail.com>
2016-02-26 15:59:22 +0300

    Added files via upload
    Script for marker coordinates transfer to it's parent by marker name.

---
+++ Streaming/MarkerTaker.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace QualisysRealTime.Unity
+{
+
+	class MarkerTaker : MonoBehaviour
+	{
+	
+		public string markerName;
+
+		private List<LabeledMarker> markerData;
+		private RTClient rtClient;
+		private GameObject markerRoot;
+		private Vector3 markerPosition = new Vector3();
+		private bool streaming = false;
+
+		// Use this for initialization
+		void Start()
+		{
+			rtClient = RTClient.GetInstance();
+			markerRoot = gameObject;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+			if (rtClient == null) rtClient = RTClient.GetInstance();
+			if (rtClient.GetStreamingStatus() && !streaming)
+			{
+				streaming = true;
+			}
+			if (!rtClient.GetStreamingStatus() && streaming)
+			{
+				streaming = false;
+			}
+
+			markerData = rtClient.Markers;
+			if (markerData == null && markerData.Count == 0)
+				return;
+			for (int i = 0; i < markerData.Count; i++)
+			{
+					if (markerData[i].Label == markerName)
+					{
+						markerPosition = markerData[i].Position;
+						transform.position = markerPosition;
+					}
+			}
+		}
+	}
+}
