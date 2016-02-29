3f6fd655bcbd5f1232097b9ba87e0612df457597
Alec <frstrtr@gmail.com>
2016-02-29 09:42:23 +0300

    Update RTMarkerStream.cs

--- Streaming/RTMarkerStream.cs
+++ Streaming/RTMarkerStream.cs
@@ -1,96 +1,58 @@
-﻿// Unity SDK for Qualisys Track Manager. Copyright 2015 Qualisys AB
-//
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
-using QTMRealTimeSDK;
+//#IISCI
+//Script for marker coordinates transfer to it's parent by marker name.
 
 namespace QualisysRealTime.Unity
 {
-    public class RTMarkerStream : MonoBehaviour
-    {
-        private List<LabeledMarker> markerData;
-        private RTClient rtClient;
-        private GameObject markerRoot;
-        private List<GameObject> markers;
 
-        public bool visibleMarkers = true;
+	class RTMarkerTaker : MonoBehaviour
+	{
 
-        public float markerScale = 0.01f;
+		public string markerName = "Put QTM marker name here";
 
-        private bool streaming = false;
+		private List<LabeledMarker> markerData;
+		private RTClient rtClient;
+		private GameObject markerRoot;
+		private Vector3 markerPosition = new Vector3();
+		private bool streaming = false;
 
-        // Use this for initialization
-        void Start()
-        {
-            rtClient = RTClient.GetInstance();
-            markers = new List<GameObject>();
-            markerRoot = this.gameObject;
-        }
+		// Use this for initialization
+		void Start()
+		{
+			rtClient = RTClient.GetInstance();
+			markerRoot = gameObject;
+		}
 
+		// Update is called once per frame
+		void Update()
+		{
 
-        private void InitiateMarkers()
-        {
-            foreach (var marker in markers)
-            {
-                UnityEngine.GameObject.Destroy(marker);
-            }
+			if (rtClient == null) rtClient = RTClient.GetInstance();
+			if (rtClient.GetStreamingStatus() && !streaming)
+			{
+				streaming = true;
+			}
+			if (!rtClient.GetStreamingStatus() && streaming)
+			{
+				streaming = false;
+			}
 
-            markers.Clear();
-            markerData = rtClient.Markers;
+			//Seeking fo desired marker name
 
-            for (int i = 0; i < markerData.Count; i++)
-            {
-                GameObject newMarker = UnityEngine.GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                newMarker.name = markerData[i].Label;
-                newMarker.transform.parent = markerRoot.transform;
-                newMarker.transform.localScale = Vector3.one * markerScale;
-                newMarker.SetActive(false);
-                markers.Add(newMarker);
-            }
-        }
+			markerData = rtClient.Markers;
+			if (markerData == null && markerData.Count == 0)
+				return;
+			for (int i = 0; i < markerData.Count; i++)
+			{
+				if (markerData[i].Label == markerName)
+				{
+					//Transfering marker position to parented object
 
-        // Update is called once per frame
-        void Update()
-        {
-            if (rtClient.GetStreamingStatus() && !streaming)
-            {
-                InitiateMarkers();
-                streaming = true;
-            }
-            if (!rtClient.GetStreamingStatus() && streaming)
-            {
-                streaming = false;
-                InitiateMarkers();
-            }
-
-            markerData = rtClient.Markers;
-
-            if (markerData == null && markerData.Count == 0)
-                return;
-
-            if (markers.Count != markerData.Count)
-            {
-                InitiateMarkers();
-            }
-
-            for (int i = 0; i < markerData.Count; i++)
-            {
-                if (markerData[i].Position.magnitude > 0)
-                {
-                    markers[i].name = markerData[i].Label;
-                    markers[i].GetComponent<Renderer>().material.color = markerData[i].Color;
-                    markers[i].transform.localPosition = markerData[i].Position;
-                    markers[i].SetActive(true);
-                    markers[i].GetComponent<Renderer>().enabled = visibleMarkers;
-                    markers[i].transform.localScale = Vector3.one * markerScale;
-                }
-                else
-                {
-                    //hide markers if we cant find them.
-                    markers[i].SetActive(false);
-                }
-            }
-        }
-    }
+					markerPosition = markerData[i].Position;
+					transform.position = markerPosition;
+				}
+			}
+		}
+	}
 }
