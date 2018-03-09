22385e7e7872e08e300d44eadb3976653ff9238e
Morgan Larsson <morgan.larsson@qualisys.se>
2018-03-09 08:38:11 +0100

    Added demo assets

---
+++ Qualisys/DemoAssets/UI/CreateScrollList.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using QualisysRealTime.Unity;
+using QTMRealTimeSDK.Data;
+using QTMRealTimeSDK;
+using QTMRealTimeSDK.Network;
+
+public class CreateScrollList : MonoBehaviour {
+
+    public GameObject serverButtonPrefab; 
+    public Transform contentPanel;
+    private List<DiscoveryResponse> discoveryResponses;
+
+    void Update()
+    {
+        if (RTClient.GetInstance().GetStreamingStatus())
+        {
+            Disable();
+        }
+
+        if (contentPanel.childCount < 1)
+        {
+            UpdateList();
+        }
+    }
+    public void UpdateList()
+    {
+        for (int i = 0; i < contentPanel.childCount; i++)
+        {
+            Destroy(contentPanel.GetChild(i).gameObject);
+        }
+        discoveryResponses = RTClient.GetInstance().GetServers();
+        foreach (DiscoveryResponse server in discoveryResponses)
+        {
+            GameObject newServer = Instantiate(serverButtonPrefab) as GameObject;
+            ServerButton serverButton = newServer.GetComponent<ServerButton>();
+            serverButton.HostText.text = server.HostName;
+            serverButton.IpAddressText.text = server.IpAddress + ":" + server.Port;
+            serverButton.InfoText.text = server.InfoText;
+            serverButton.response = server;
+            newServer.transform.SetParent(contentPanel);
+        }
+    }
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Qualisys/DemoAssets/UI/ServerButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using QTMRealTimeSDK.Data;
+using QualisysRealTime.Unity;
+using QTMRealTimeSDK;
+
+[System.Serializable]
+public class ServerButton : MonoBehaviour {
+    public Button button;
+    public Text HostText;
+    public Text IpAddressText;
+    public Text InfoText;
+    public DiscoveryResponse response;
+    void Start()
+    {
+        transform.localScale = Vector3.one;
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = Vector3.zero;
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(new UnityEngine.Events.UnityAction(Connect));
+    }
+    void Connect()
+    {
+        if (!RTClient.GetInstance().Connect(response, response.Port, true, true, true))
+        {
+            InfoText.color = Color.red;
+            InfoText.text = "Could not connect to this server";
+        }
+        else
+        {
+            SendMessageUpwards("Disable");
+        }
+    }
+}
