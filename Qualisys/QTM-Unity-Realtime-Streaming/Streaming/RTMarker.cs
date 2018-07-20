d8a5679cdd52edf5c1afa95d910562dff7d6c1d4
Morgan Larsson <morgan.larsson@qualisys.se>
2018-07-20 12:48:57 +0200

    Added a RTMarker example script to take position of one marker and set position of the associated game object

---
+++ Qualisys/QTM-Unity-Realtime-Streaming/Streaming/RTMarker.cs
@@ -0,0 +1,45 @@
+﻿// Unity SDK for Qualisys Track Manager. Copyright 2015-2018 Qualisys AB
+//
+using UnityEngine;
+using System.Collections;
+using QTMRealTimeSDK;
+
+namespace QualisysRealTime.Unity
+{
+    class RTMarker : MonoBehaviour
+    {
+        public string MarkerName = "Put QTM Marker name here";
+        public Vector3 PositionOffset = new Vector3(0, 0, 0);
+
+        protected RTClient rtClient;
+        protected LabeledMarker marker;
+
+        public virtual void applyMarkerTransform()
+        {
+            if (marker.Position.magnitude > 0) //just to avoid error when position is NaN
+            {
+                transform.position = marker.Position + PositionOffset;
+                if (transform.parent)
+                    transform.position += transform.parent.position;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            rtClient = RTClient.GetInstance();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rtClient == null) rtClient = RTClient.GetInstance();
+
+            marker = rtClient.GetMarker(MarkerName);
+            if (marker != null)
+            {
+                this.applyMarkerTransform();
+            }
+        }
+    }
+}
